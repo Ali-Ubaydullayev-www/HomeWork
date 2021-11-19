4ed9eb45ab3f9c20c95457a6b1901a99bead8f2a
Ali <65356558+Ali-Ubaydullayev-www@users.noreply.github.com>
2021-11-20 00:18:59 +0500

    Add files via upload

--- scrip2.js
+++ scrip2.js
@@ -1,31 +1,51 @@
-function obj(num1, num2) {
-    if (num1, num2) {
-        console.log(num1 + num2);
-    }
-    if  (num1) {
-        return function (num2) {
-            console.log(num1 + num2);
-        }
-    }
-}
-obj(1)(2);
-obj(1, 2);
+// function obj(num1, num2) {
+//     if (num1, num2) {
+//         console.log(num1 + num2);
+//     }
+//     if  (num1) {
+//         return function (num2) {
+//             console.log(num1 + num2);
+//         }
+//     }
+// }
+// obj(2)(5);
+// obj(1, 2);
 
 
 
 
-const obj1 = {
-    a : 1,
-    b : 2,
-    getA(){
-        console.log(this.a);
-        return this
-    },
-    getB(){
-        console.log(this.b);
-    },
-}
 
-obj1.getA().getB()
+// const obj1 = {
+//     a : 1,
+//     b : 2,
+//     getA(){
+//         console.log(this.a);
+//         return this
+//     },
+//     getB(){
+//         console.log(this.b);
+//     },
+// }
+
+// obj1.getA().getB()
 
 
+// ******************* первый способ 1
+
+
+
+// var a = '4654365352196'
+// result = 0;
+// for (let i = 0; i < a.length; i++)result = +(result) + +(a[i])
+// console.log(result);
+
+
+// ******************* второй способ 2
+
+
+
+
+// b = [15, 135, 1, 5, 9, 45,65]
+// result2 = 0
+// for (let j = 0; j < b.length; j++)result2 = +(result2) + +(b[j])
+// console.log(result2);
